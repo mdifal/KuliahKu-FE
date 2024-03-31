0df628a097fb346c689007ec6f7994a51ada5750
Fauzanaylassana98 <Fauzanaylassana98>
2024-03-31 20:42:33 +0700

    update timer.dart

--- lib/ui/views/timer.dart
+++ lib/ui/views/timer.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:kuliahku/ui/shared/style.dart';
 import 'package:kuliahku/ui/widgets/button.dart';
+import 'package:kuliahku/ui/widgets/dropdown.dart';
 import 'dart:math' as math;
 
 class TimerPage extends StatefulWidget {
@@ -16,6 +17,9 @@ class _TimerPageState extends State<TimerPage> {
   bool _isRunning = false;
   late Timer _timer;
 
+  String? _selectedCourse;
+  String? _selectedLearningType;
+
   void _startTimer() {
     _timer = Timer.periodic(Duration(seconds: 1), (timer) {
       setState(() {
@@ -25,7 +29,7 @@ class _TimerPageState extends State<TimerPage> {
     _isRunning = true;
   }
 
-  void _pauseTimer() {
+  void _stopTimer() {
     _timer.cancel();
     _isRunning = false;
   }
@@ -63,14 +67,13 @@ class _TimerPageState extends State<TimerPage> {
         title: Text(
           'Timer',
           style: TextStyle(
-            color: white,
+            color: Colors.white,
             fontFamily: 'Poppins',
             fontSize: 24,
             fontWeight: FontWeight.w600,
-          ), // Set color of AppBar text to white
+          ),
         ),
-        iconTheme: IconThemeData(color: white),
-        backgroundColor: mainColor,
+        backgroundColor: Colors.blue,
       ),
       body: Center(
         child: Column(
@@ -86,7 +89,7 @@ class _TimerPageState extends State<TimerPage> {
                     shape: BoxShape.circle,
                     color: Colors.transparent,
                     border: Border.all(
-                      color: lightBlue,
+                      color: Colors.lightBlue,
                       width: 8,
                     ),
                   ),
@@ -99,25 +102,65 @@ class _TimerPageState extends State<TimerPage> {
                 ),
               ],
             ),
-            SizedBox(height: 100),
+            SizedBox(height: 30),
+            _isRunning
+                ? Column(
+              children: [
+                Text(
+                  'Mata Kuliah: $_selectedCourse',
+                  style: TextStyle(fontSize: 20),
+                ),
+                Text(
+                  'Jenis Belajar: $_selectedLearningType',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ],
+            )
+                : Column(
+              children: [
+                CustomDropdown(
+                  label: "Mata Kuliah",
+                  placeholder: "Pilih mata kuliah",
+                  items: ['Basis Data', 'Matematika'],
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedCourse = value;
+                    });
+                  },
+                ),
+                SizedBox(height: 5),
+                CustomDropdown(
+                  label: "Jenis Belajar",
+                  placeholder: "Pilih jenis belajar",
+                  items: ['Mengerjakan tugas', 'Belajar Mandiri'],
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedLearningType = value;
+                    });
+                  },
+                ),
+              ],
+            ),
+            SizedBox(height: 30),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 CustomIconsButton(
-                  label: _isRunning ? 'Pause' : 'Start',
-                  iconData: _isRunning ? Icons.pause : Icons.play_arrow,
-                  onPressed: _isRunning ? _pauseTimer : _startTimer,
-                  textColor: lightBlue,
-                  backgroundColor: white,
+                  label: _isRunning ? 'Stop' : 'Start',
+                  iconData: _isRunning ? Icons.stop : Icons.play_arrow,
+                  onPressed: _isRunning ? _stopTimer : _startTimer,
+                  textColor: Colors.lightBlue,
+                  backgroundColor: Colors.white,
                 ),
                 SizedBox(width: 20),
-                CustomIconsButton(
-                  onPressed: _resetTimer,
-                  iconData: Icons.refresh,
-                  label: 'Reset',
-                  textColor: lightBlue,
-                  backgroundColor: white,
-                ),
+                if (_isRunning) // Tambahkan kondisi ini
+                  CustomIconsButton(
+                    onPressed: _resetTimer,
+                    iconData: Icons.refresh,
+                    label: 'Reset',
+                    textColor: Colors.lightBlue,
+                    backgroundColor: Colors.white,
+                  ),
               ],
             ),
           ],
