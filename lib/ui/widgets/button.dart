cb20860c69cae11d2149e94fd3d4a74d0c9b9fa4
Fauzanaylassana98 <Fauzanaylassana98>
2024-03-31 20:21:21 +0700

    add customiconsbutton

--- lib/ui/widgets/button.dart
+++ lib/ui/widgets/button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kuliahku/ui/shared/style.dart';
+import 'package:kuliahku/ui/shared/style.dart';
 
 class CustomButton extends StatelessWidget {
   final String? label;
@@ -28,8 +29,60 @@ class CustomButton extends StatelessWidget {
           ),
           child: Text(
             label!,
-            style: TextStyle(color: textColor), // Warna teks putih
+            style: TextStyle(
+              color: textColor,
+              fontFamily: 'Poppins',
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+            ),
           ),
         ));
+  }
+}
+
+class CustomIconsButton extends StatelessWidget {
+  final String? label;
+  final IconData? iconData;
+  final Color? backgroundColor;
+  final Color? textColor;
+  final VoidCallback? onPressed;
+
+  const CustomIconsButton({
+    Key? key,
+    this.label,
+    this.iconData,
+    this.backgroundColor,
+    this.textColor,
+    this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(5),
+      child: ElevatedButton.icon(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          backgroundColor: backgroundColor,
+          minimumSize: Size(150, 50),
+        ),
+        icon: Icon(
+          iconData,
+          color: textColor,
+        ),
+        label: Text(
+          label!,
+          style: TextStyle(
+            color: textColor,
+            fontFamily: 'Poppins',
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+    );
   }
 }
