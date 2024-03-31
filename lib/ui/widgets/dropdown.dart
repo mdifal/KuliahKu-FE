985d191bd0f66d2c55ce169f253579cfffe2edd3
Fauzanaylassana98 <Fauzanaylassana98>
2024-03-31 21:17:10 +0700

    update widgets drop down

--- lib/ui/widgets/dropdown.dart
+++ lib/ui/widgets/dropdown.dart
@@ -6,12 +6,14 @@ class CustomDropdown extends StatefulWidget {
   final List<String> items;
   final String? label;
   final String? placeholder;
+  final ValueChanged<String?>? onChanged;
 
   const CustomDropdown({
     Key? key,
     this.placeholder,
     this.label,
     required this.items,
+    this.onChanged,
   }) : super(key: key);
 
   @override
@@ -24,79 +26,83 @@ class _CustomDropdownState extends State<CustomDropdown> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: EdgeInsets.all(8),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(
-                  bottom: 8.0), // Tambahkan margin bawah pada teks
-              child: Text(
-                widget.label!,
-                style: TextStyle(
-                  color: black,
-                  fontFamily: 'Poppins',
-                  fontSize: 12,
-                  fontWeight: FontWeight.w600,
-                ),
+      margin: EdgeInsets.all(8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(
+                bottom: 8.0), // Tambahkan margin bawah pada teks
+            child: Text(
+              widget.label!,
+              style: TextStyle(
+                color: black,
+                fontFamily: 'Poppins',
+                fontSize: 12,
+                fontWeight: FontWeight.w600,
               ),
             ),
-            DropdownButtonFormField2<String>(
-              decoration: InputDecoration(
-                contentPadding: const EdgeInsets.symmetric(vertical: 6),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15),
+          ),
+          DropdownButtonFormField2<String>(
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.symmetric(vertical: 6),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+            ),
+            hint: Text(
+              widget.placeholder ?? '',
+              style: const TextStyle(fontSize: 14),
+            ),
+            items: widget.items
+                .map((item) => DropdownMenuItem<String>(
+              value: item,
+              child: Text(
+                item,
+                style: const TextStyle(
+                  fontSize: 14,
                 ),
               ),
-              hint: Text(
-                widget.placeholder ?? '',
-                style: const TextStyle(fontSize: 14),
-              ),
-              items: widget.items
-                  .map((item) => DropdownMenuItem<String>(
-                        value: item,
-                        child: Text(
-                          item,
-                          style: const TextStyle(
-                            fontSize: 14,
-                          ),
-                        ),
-                      ))
-                  .toList(),
-              validator: (value) {
-                if (value == null) {
-                  return widget.placeholder;
-                }
-                return null;
-              },
-              onChanged: (value) {
-                setState(() {
-                  selectedValue = value;
-                });
-              },
-              onSaved: (value) {
+            ))
+                .toList(),
+            validator: (value) {
+              if (value == null) {
+                return widget.placeholder;
+              }
+              return null;
+            },
+            onChanged: (value) {
+              setState(() {
                 selectedValue = value;
-              },
-              buttonStyleData: const ButtonStyleData(
-                padding: EdgeInsets.only(right: 8),
+              });
+              if (widget.onChanged != null) {
+                widget.onChanged!(value);
+              }
+            },
+            onSaved: (value) {
+              selectedValue = value;
+            },
+            buttonStyleData: const ButtonStyleData(
+              padding: EdgeInsets.only(right: 8),
+            ),
+            iconStyleData: const IconStyleData(
+              icon: Icon(
+                Icons.arrow_drop_down,
+                color: Colors.black45,
               ),
-              iconStyleData: const IconStyleData(
-                icon: Icon(
-                  Icons.arrow_drop_down,
-                  color: Colors.black45,
-                ),
-                iconSize: 24,
+              iconSize: 24,
+            ),
+            dropdownStyleData: DropdownStyleData(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
               ),
-              dropdownStyleData: DropdownStyleData(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                ),
-              ),
-              menuItemStyleData: const MenuItemStyleData(
-                padding: EdgeInsets.symmetric(horizontal: 16),
-              ),
-            )
-          ],
-        ));
+            ),
+            menuItemStyleData: const MenuItemStyleData(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
