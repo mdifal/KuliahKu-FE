b63d23d548d9313d65a0079ba18111e0ed98438f
nisrinawafaz <nisrinawafaz@gmail.com>
2024-03-31 19:19:08 +0700

    update tambah jadwal page dan widget dropdown

--- lib/ui/views/tambahJadwal.dart
+++ lib/ui/views/tambahJadwal.dart
@@ -17,65 +17,86 @@ class _tambahJadwalPageState extends State<tambahJadwalPage> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('lala'),
-        ),
-        body: Container(
-          padding: EdgeInsets.all(30.0),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20.0),
-              topRight: Radius.circular(20.0),
-            ),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+          title: Row(
             children: [
-              CustomTextField(
-                label: "Nama Mata Kuliah",
-                password: false,
-                placeholder: "contoh : basis data",
-              ),
-              Padding(
-                  padding: EdgeInsets.only(left: 8, right: 8),
-                  child: CustomDropdown(
-                      label: "Hari",
-                      placeholder: "Select the day",
-                      items: ['Male', 'Female'])),
-              Padding(
-  padding: EdgeInsets.only(left: 8, right: 8),
-  child: Row(
-    children: [
-      Expanded(
-        child: CustomDropdown(
-          label: "Hari",
-          placeholder: "Jam Mulai",
-          items: ['Male', 'Female']
-        ),
-      ),
-      Expanded(
-        child: CustomDropdown(
-          label: "Hari",
-          placeholder: "Jam Selesai",
-          items: ['Male', 'Female']
-        ),
-      ),
-    ],
-  ),
-),
-              CustomTextField(
-                label: "Dosen Pengampu",
-                password: false,
-                placeholder: "contoh : Bapak fulan",
-              ),
-              CustomTextField(
-                label: "Ruangan",
-                password: false,
-                placeholder: "contoh : R-109",
+              Icon(Icons.arrow_back, color: Colors.white), // Ikon di depan teks
+              SizedBox(width: 8), // Spasi antara ikon dan teks
+              Text(
+                'Input Jadwal Baru',
+                style: TextStyle(color: Colors.white),
               ),
             ],
           ),
+          backgroundColor: mainColor,
         ),
+        body: Container(
+            padding: EdgeInsets.all(30.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20.0),
+                topRight: Radius.circular(20.0),
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    CustomTextField(
+                      label: "Nama Mata Kuliah",
+                      password: false,
+                      placeholder: "contoh : basis data",
+                    ),
+                    Padding(
+                        padding: EdgeInsets.only(left: 4, right: 4),
+                        child: CustomDropdown(
+                            label: "Hari",
+                            placeholder: "Select the day",
+                            items: [
+                              'Senin',
+                              'Selasa',
+                              'Rabu',
+                              'Kamis',
+                              'Jumat',
+                              'Sabtu'
+                            ])),
+                    Padding(
+                      padding: EdgeInsets.only(left: 4, right: 4),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: CustomDropdown(
+                                label: "Jam Mulai",
+                                placeholder: "Select the time",
+                                items: ['07.00', '08.40']),
+                          ),
+                          Expanded(
+                            child: CustomDropdown(
+                                label: "Jam Selesai",
+                                placeholder: "Select the time",
+                                items: ['07.00', '08.40']),
+                          ),
+                        ],
+                      ),
+                    ),
+                    CustomTextField(
+                      label: "Dosen Pengampu",
+                      password: false,
+                      placeholder: "contoh : Bapak fulan",
+                    ),
+                    CustomTextField(
+                      label: "Ruangan",
+                      password: false,
+                      placeholder: "contoh : R-109",
+                    ),
+                  ],
+                ),
+                CustomButton(
+                    label: "Simpan", backgroundColor: yellow, textColor: black)
+              ],
+            )),
       ),
     );
   }
--- lib/ui/widgets/dropdown.dart
+++ lib/ui/widgets/dropdown.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:dropdown_button2/dropdown_button2.dart';
+import 'package:kuliahku/ui/shared/style.dart';
 
 class CustomDropdown extends StatefulWidget {
   final List<String> items;
@@ -22,60 +23,80 @@ class _CustomDropdownState extends State<CustomDropdown> {
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButtonFormField2<String>(
-      decoration: InputDecoration(
-        contentPadding: const EdgeInsets.symmetric(vertical: 16),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(15),
-        ),
-      ),
-      hint: Text(
-        widget.placeholder ?? '',
-        style: const TextStyle(fontSize: 14),
-      ),
-      items: widget.items
-          .map((item) => DropdownMenuItem<String>(
-                value: item,
-                child: Text(
-                  item,
-                  style: const TextStyle(
-                    fontSize: 14,
-                  ),
+    return Container(
+        margin: EdgeInsets.all(8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(
+                  bottom: 8.0), // Tambahkan margin bawah pada teks
+              child: Text(
+                widget.label!,
+                style: TextStyle(
+                  color: black,
+                  fontFamily: 'Poppins',
+                  fontSize: 12,
+                  fontWeight: FontWeight.w600,
                 ),
-              ))
-          .toList(),
-      validator: (value) {
-        if (value == null) {
-          return 'Please select gender.';
-        }
-        return null;
-      },
-      onChanged: (value) {
-        setState(() {
-          selectedValue = value;
-        });
-      },
-      onSaved: (value) {
-        selectedValue = value;
-      },
-      buttonStyleData: const ButtonStyleData(
-        padding: EdgeInsets.only(right: 8),
-      ),
-      iconStyleData: const IconStyleData(
-        icon: Icon(
-          Icons.arrow_drop_down,
-          color: Colors.black45,
-        ),
-        iconSize: 24,
-      ),
-      dropdownStyleData: DropdownStyleData(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-        ),
-      ),
-      menuItemStyleData: const MenuItemStyleData(
-        padding: EdgeInsets.symmetric(horizontal: 16),
-      ),
-    );
+              ),
+            ),
+            DropdownButtonFormField2<String>(
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.symmetric(vertical: 6),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+              hint: Text(
+                widget.placeholder ?? '',
+                style: const TextStyle(fontSize: 14),
+              ),
+              items: widget.items
+                  .map((item) => DropdownMenuItem<String>(
+                        value: item,
+                        child: Text(
+                          item,
+                          style: const TextStyle(
+                            fontSize: 14,
+                          ),
+                        ),
+                      ))
+                  .toList(),
+              validator: (value) {
+                if (value == null) {
+                  return widget.placeholder;
+                }
+                return null;
+              },
+              onChanged: (value) {
+                setState(() {
+                  selectedValue = value;
+                });
+              },
+              onSaved: (value) {
+                selectedValue = value;
+              },
+              buttonStyleData: const ButtonStyleData(
+                padding: EdgeInsets.only(right: 8),
+              ),
+              iconStyleData: const IconStyleData(
+                icon: Icon(
+                  Icons.arrow_drop_down,
+                  color: Colors.black45,
+                ),
+                iconSize: 24,
+              ),
+              dropdownStyleData: DropdownStyleData(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+              menuItemStyleData: const MenuItemStyleData(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+              ),
+            )
+          ],
+        ));
   }
 }
