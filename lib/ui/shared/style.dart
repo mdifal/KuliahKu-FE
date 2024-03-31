a54df55f19a8afafda9888bdc58403d78b2a3649
nisrinawafaz <nisrinawafaz@gmail.com>
2024-03-31 13:32:32 +0700

    add color

--- lib/ui/shared/style.dart
+++ lib/ui/shared/style.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 // Colors
-const Color mainColor = Color(0xFFF05B5D);
+const Color mainColor = Color(0xFF436CD3);
+const Color yellow = Color(0xFFFFCB46);
 const Color white = Color(0xFFFFFFFF);
 const Color black = Color(0xFF020202);
 const Color blackSoft = Color(0xFF535353);
