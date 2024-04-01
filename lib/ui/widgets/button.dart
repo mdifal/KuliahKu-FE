43836b9692eac72a5c6781e8b05e043f991af529
Fauzanaylassana98 <fauza.naylassana.tif22@polban.ac.id>
2024-04-01 16:18:19 +0700

    add button upload file

--- lib/ui/widgets/button.dart
+++ lib/ui/widgets/button.dart
@@ -6,37 +6,40 @@ class CustomButton extends StatelessWidget {
   final String? label;
   final Color? backgroundColor;
   final Color? textColor;
+  final VoidCallback? onPressed;
 
-  const CustomButton(
-      {Key? key, this.label, this.backgroundColor, this.textColor})
-      : super(key: key);
+  const CustomButton({
+    Key? key,
+    this.label,
+    this.backgroundColor,
+    this.textColor,
+    this.onPressed,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: EdgeInsets.all(5),
-        child: ElevatedButton(
-          onPressed: () {
-            // Logika untuk saat tombol ditekan
-          },
-          style: ElevatedButton.styleFrom(
-            shape: RoundedRectangleBorder(
-              borderRadius:
-                  BorderRadius.circular(12), // Ubah nilai sesuai kebutuhan Anda
-            ),
-            backgroundColor: backgroundColor,
-            minimumSize: Size(150, 50),
+      margin: EdgeInsets.all(5),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
           ),
-          child: Text(
-            label!,
-            style: TextStyle(
-              color: textColor,
-              fontFamily: 'Poppins',
-              fontSize: 18,
-              fontWeight: FontWeight.w600,
-            ),
+          backgroundColor: backgroundColor,
+          minimumSize: Size(150, 50),
+        ),
+        child: Text(
+          label!,
+          style: TextStyle(
+            color: textColor,
+            fontFamily: 'Poppins',
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
 
@@ -79,6 +82,45 @@ class CustomIconsButton extends StatelessWidget {
             color: textColor,
             fontFamily: 'Poppins',
             fontSize: 18,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomUploadFileButton extends StatelessWidget {
+  final String? label;
+  final VoidCallback? onPressed;
+
+  const CustomUploadFileButton({
+    Key? key,
+    this.label,
+    this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(5),
+      height: 30,
+      width: 50,
+      child: ElevatedButton.icon(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          backgroundColor: white,
+        ),
+        icon: Icon(Icons.attach_file, color: darkBlue),
+        label: Text(
+          label!,
+          style: TextStyle(
+            color: darkBlue,
+            fontFamily: 'Poppins',
+            fontSize: 14,
             fontWeight: FontWeight.w600,
           ),
         ),
