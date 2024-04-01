b9bcafcfc8feed708cb7027e72524e9d9f13075d
Fauzanaylassana98 <fauza.naylassana.tif22@polban.ac.id>
2024-04-01 16:17:55 +0700

    update landing page

--- lib/ui/views/landing.dart
+++ lib/ui/views/landing.dart
@@ -2,76 +2,102 @@ import 'package:flutter/material.dart';
 import 'package:kuliahku/ui/shared/images.dart';
 import 'package:kuliahku/ui/shared/style.dart';
 import 'package:kuliahku/ui/widgets/button.dart';
+import 'package:kuliahku/ui/views/login.dart';
+import 'package:kuliahku/ui/views/register.dart';
 
-class LandingPage extends StatefulWidget {
+class LandingPage extends StatelessWidget {
   const LandingPage({Key? key}) : super(key: key);
 
   @override
-  _LandingPageState createState() => _LandingPageState(); // Perhatikan bahwa ini harus mengembalikan _LandingPageState
-}
-
-class _LandingPageState extends State<LandingPage> { // Perhatikan perubahan _LoginPageState menjadi _LandingPageState
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: mainColor, // Mengatur warna latar belakang
-      body: SafeArea(
-        child: ListView(
-          children: <Widget>[
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: 300, // Sesuaikan dengan tinggi yang diinginkan
-              child: Image.asset(
-                background_landing,
-                fit: BoxFit.cover, // Opsional: mengatur gambar agar memenuhi ukuran kotak
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: mainColor,
+        body: SafeArea(
+          child: ListView(
+            children: <Widget>[
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 300,
+                child: Image.asset(
+                  background_landing,
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-            Column(
-              children: <Widget>[
-                const SizedBox(height: 80.0),
-                RichText(
-                  text: TextSpan(
-                    children: [
-                      TextSpan(
-                        text: 'Kuliah',
-                        style: TextStyle(
-                          fontFamily: 'Poppins',
-                          fontSize: 52,
-                          fontWeight: FontWeight.w600,
-                          color: white,
+              Column(
+                children: <Widget>[
+                  const SizedBox(height: 80.0),
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: 'Kuliah',
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 52,
+                            fontWeight: FontWeight.w600,
+                            color: white,
+                          ),
                         ),
-                      ),
-                      TextSpan(
-                        text: 'Ku',
-                        style: TextStyle(
-                          fontFamily: 'Poppins',
-                          fontSize: 52,
-                          fontWeight: FontWeight.w600,
-                          color: yellow, // Mengatur warna teks menjadi kuning
+                        TextSpan(
+                          text: 'Ku',
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 52,
+                            fontWeight: FontWeight.w600,
+                            color: yellow,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  'Nikmati kuliahmu, nikmati hidupmu!',
-                  style: TextStyle(
-                    fontFamily: 'Poppins',
-                    fontSize: 20,
-                    fontWeight: FontWeight.w600,
-                    color: white,// Mengatur warna teks menjadi kuning
+                  SizedBox(height: 10),
+                  Text(
+                    'Nikmati kuliahmu, nikmati hidupmu!',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                      color: white,
+                    ),
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 80.0),
-            CustomButton(label: 'Login', backgroundColor: white, textColor: black,),
-            const SizedBox(height: 10.0),
-            CustomButton(label: 'Create a new account', backgroundColor: yellow, textColor: black)
-          ],
+                ],
+              ),
+              const SizedBox(height: 80.0),
+              CustomButton(
+                label: 'Login',
+                backgroundColor: white,
+                textColor: black,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginPage()), // Navigasi ke halaman Register
+                  );
+                },
+              ),
+              const SizedBox(height: 10.0),
+              CustomButton(
+                label: 'Create a new account',
+                backgroundColor: yellow,
+                textColor: black,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => RegisterPage()), // Navigasi ke halaman Register
+                  );
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+void _navigateToLogin(BuildContext context) {
+  Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) => LoginPage()),
+  );
 }
