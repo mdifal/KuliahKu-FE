0c671970bb7ad8f8efa4f3d6287472193b34d4c4
nisrinawafaz <nisrinawafaz@gmail.com>
2024-04-15 23:47:09 +0700

    Add widget card history dan page history record

---
+++ lib/ui/views/history_time_record.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:kuliahku/ui/shared/style.dart';
+import 'package:kuliahku/ui/widgets/card_history.dart';
+import 'package:kuliahku/ui/widgets/dropdown.dart';
+import 'package:kuliahku/ui/widgets/text_field.dart';
+import 'package:kuliahku/ui/widgets/button.dart';
+
+class HistoryRecordPage extends StatefulWidget {
+  const HistoryRecordPage({super.key});
+
+  @override
+  State<HistoryRecordPage> createState() => _HistoryRecordPageState();
+}
+
+class _HistoryRecordPageState extends State<HistoryRecordPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Row(
+              children: [
+                IconButton(
+                  icon: Icon(
+                    Icons.arrow_back,
+                    color: white,
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                SizedBox(width: 8),
+                Text(
+                  'History Time Record',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+            backgroundColor: mainColor,
+          ),
+          body: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
+                ),
+              ),
+              child: Column(
+                children: [
+                  CardHistory(
+                    title: 'History 1',
+                    mataKuliah: 'Database',
+                    time: 120, // Misalkan waktu dalam menit
+                  ),
+                  SizedBox(height: 16),
+                  CardHistory(
+                    title: 'History 1',
+                    mataKuliah: 'Database',
+                    time: 7200, // Misalkan waktu dalam menit
+                  ),
+                  SizedBox(height: 16),
+                  CardHistory(
+                    title: 'History 1',
+                    mataKuliah: 'Database',
+                    time: 3600, // Misalkan waktu dalam menit
+                  ),
+                  SizedBox(height: 16),
+                  CardHistory(
+                    title: 'History 1',
+                    mataKuliah: 'Database',
+                    time: 1900, // Misalkan waktu dalam menit
+                  ),
+                  SizedBox(height: 16),
+                  CardHistory(
+                    title: 'History 1',
+                    mataKuliah: 'Database',
+                    time: 120, // Misalkan waktu dalam menit
+                  ),
+                  SizedBox(height: 16),
+                  CardHistory(
+                    title: 'History 1',
+                    mataKuliah: 'Database',
+                    time: 120, // Misalkan waktu dalam menit
+                  )
+                ],
+              ),
+            ),
+          )),
+    );
+  }
+}
---
+++ lib/ui/widgets/card_history.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:kuliahku/ui/shared/style.dart';
+
+// Fungsi untuk memformat durasi
+String formatDuration(int seconds) {
+  int hours = seconds ~/ 3600;
+  int minutes = (seconds % 3600) ~/ 60;
+  int remainingSeconds = seconds % 60;
+
+  String hoursString = hours > 0 ? '$hours jam ' : '';
+  String minutesString = minutes > 0 ? '$minutes menit ' : '';
+  String secondsString = '$remainingSeconds detik';
+
+  return '$hoursString$minutesString$secondsString';
+}
+
+Color getTimeColor(int? seconds) {
+      if (seconds != null) {
+        if (seconds >= 7200){
+          return cardGreen; 
+        }
+        else if (seconds >= 3600) {
+          // Lebih dari atau sama dengan 1 jam
+          return cardBlue; 
+        } else if (seconds > 1800) {
+          // Lebih dari 30 menit
+          return cardYellow;
+        } else if (seconds > 0) {
+          // Lebih dari 0 detik
+          return cardOrange; 
+        }
+      }
+      return Color(0xFF717171); // Grey jika null atau 0
+    }
+
+class CardHistory extends StatelessWidget {
+  final String? title;
+  final String? mataKuliah;
+  final int? time;
+
+  const CardHistory({Key? key, this.title, this.mataKuliah, this.time})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    String formattedTime = formatDuration(time?? 0); // Mengonversi waktu dalam detik ke format yang diinginkan
+
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 30.0),
+      decoration: BoxDecoration(
+        color: getTimeColor(time),
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title!,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+          ),
+          SizedBox(height: 5),
+          Row(
+            children: [
+              Text(
+                "Mata Kuliah: ",
+                style: TextStyle(fontSize: 15),
+              ),
+              Text(mataKuliah!, style: TextStyle(fontSize: 15)),
+            ],
+          ),
+          SizedBox(height: 5),
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(right: 8),
+                child: Icon(
+                  Icons.access_time_outlined,
+                  size: 20,
+                ),
+              ),
+              Text(
+                formattedTime,
+                style: TextStyle(fontSize: 15),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
