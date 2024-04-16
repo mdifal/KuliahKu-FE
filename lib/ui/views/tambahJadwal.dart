15dfa86a1c395b512d350971fdaf0f820429c861
nisrinawafaz <nisrinawafaz@gmail.com>
2024-04-16 11:45:29 +0700

    add date picker jadwal

--- lib/ui/views/tambahJadwal.dart
+++ lib/ui/views/tambahJadwal.dart
@@ -3,15 +3,75 @@ import 'package:kuliahku/ui/shared/style.dart';
 import 'package:kuliahku/ui/widgets/dropdown.dart';
 import 'package:kuliahku/ui/widgets/text_field.dart';
 import 'package:kuliahku/ui/widgets/button.dart';
+import 'package:time_picker_spinner/time_picker_spinner.dart';
+import 'package:intl/intl.dart';
 
 class tambahJadwalPage extends StatefulWidget {
-  const tambahJadwalPage({super.key});
+  const tambahJadwalPage({Key? key}) : super(key: key);
 
   @override
   State<tambahJadwalPage> createState() => _tambahJadwalPageState();
 }
 
 class _tambahJadwalPageState extends State<tambahJadwalPage> {
+  late DateTime startTime;
+  late DateTime endTime;
+  late String startTimeString;
+  late String endTimeString;
+
+  @override
+  void initState() {
+    super.initState();
+    startTime = DateTime.now();
+    endTime = DateTime.now();
+    updateStartTime(startTime);
+    updateEndTime(endTime);
+  }
+
+  void updateStartTime(DateTime dateTime) {
+    setState(() {
+      startTimeString = DateFormat('HH:mm:ss').format(dateTime);
+    });
+  }
+
+  void updateEndTime(DateTime dateTime) {
+    setState(() {
+      endTimeString = DateFormat('HH:mm:ss').format(dateTime);
+    });
+  }
+
+  void _showStartTimePicker(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext builder) {
+        return TimePicker(
+          onSave: (time) {
+            setState(() {
+              startTime = time;
+              updateStartTime(startTime);
+            });
+          },
+        );
+      },
+    );
+  }
+
+  void _showEndTimePicker(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext builder) {
+        return TimePicker(
+          onSave: (time) {
+            setState(() {
+              endTime = time;
+              updateEndTime(endTime);
+            });
+          },
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,8 +79,11 @@ class _tambahJadwalPageState extends State<tambahJadwalPage> {
         appBar: AppBar(
           title: Row(
             children: [
-               IconButton(
-                icon: Icon(Icons.arrow_back,color: white,),
+              IconButton(
+                icon: Icon(
+                  Icons.arrow_back,
+                  color: Colors.white,
+                ),
                 onPressed: () {
                   Navigator.pop(context);
                 },
@@ -52,36 +115,32 @@ class _tambahJadwalPageState extends State<tambahJadwalPage> {
                   password: false,
                   placeholder: "contoh : basis data",
                 ),
-                CustomDropdown(
-                    label: "Hari",
-                    placeholder: "Select the day",
-                    items: [
-                      'Senin',
-                      'Selasa',
-                      'Rabu',
-                      'Kamis',
-                      'Jumat',
-                      'Sabtu'
-                    ],
-                  ),
                 Row(
-                    children: [
-                      Expanded(
-                        child: CustomDropdown(
-                          label: "Jam Mulai",
-                          placeholder: "-",
-                          items: ['07.00', '08.40'],
-                        ),
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          _showStartTimePicker(context);
+                        },
+                        child: Text('Jam Mulai: $startTimeString'),
                       ),
-                      Expanded(
-                        child: CustomDropdown(
-                          label: "Jam Selesai",
-                          placeholder: "-",
-                          items: ['07.00', '08.40'],
-                        ),
+                    ),
+                    SizedBox(width: 8),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          _showEndTimePicker(context);
+                        },
+                        child: Text('Jam Selesai: $endTimeString'),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
+                ),
+                CustomDropdown(
+                  label: "Hari",
+                  placeholder: "Select the day",
+                  items: ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'],
+                ),
                 CustomTextField(
                   label: "Dosen Pengampu",
                   password: false,
@@ -105,6 +164,66 @@ class _tambahJadwalPageState extends State<tambahJadwalPage> {
             textColor: black,
           ),
         ),
+      ),
+    );
+  }
+}
+
+class TimePicker extends StatefulWidget {
+  final Function(DateTime) onSave;
+
+  const TimePicker({Key? key, required this.onSave}) : super(key: key);
+
+  @override
+  State<TimePicker> createState() => _TimePickerState();
+}
+
+class _TimePickerState extends State<TimePicker> {
+  late DateTime dateTime;
+
+  @override
+  void initState() {
+    super.initState();
+    dateTime = DateTime.now();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+      ),
+      height: MediaQuery.of(context).size.height / 2,
+      child: Column(
+        children: [
+          TimePickerSpinner(
+            locale: const Locale('en', ''),
+            time: dateTime,
+            is24HourMode: false,
+            isShowSeconds: true,
+            itemHeight: 80,
+            normalTextStyle: const TextStyle(
+              fontSize: 24,
+            ),
+            highlightedTextStyle:
+                const TextStyle(fontSize: 24, color: Colors.blue),
+            isForce2Digits: true,
+            onTimeChange: (time) {
+              setState(() {
+                dateTime = time;
+              });
+            },
+          ),
+          ElevatedButton(
+            onPressed: () {
+              widget.onSave(dateTime);
+              Navigator.pop(context);
+            },
+            child: Text('Simpan'),
+          ),
+        ],
       ),
     );
   }
