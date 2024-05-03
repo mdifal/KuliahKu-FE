77b1d425bb6355688b92a920284b54e249a0c533
Fauzanaylassana98 <fauza.naylassana.tif22@polban.ac.id>
2024-05-03 23:16:57 +0700

    Mendapatkan email di global dari token hasil login

--- lib/ui/shared/global.dart
+++ lib/ui/shared/global.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:kuliahku/ui/views/login.dart';
 
 // variabel
 const int userId = 1;
-const String email = "rawr@email.com";
-const String ipUrl = '192.168.0.106';
+String email = emailId;
+const String ipUrl = '192.168.100.28';
