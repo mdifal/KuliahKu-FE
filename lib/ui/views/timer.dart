eb6c2c454576862be7c9b086c7a72a8b115aeb4c
Fauzanaylassana98 <fauza.naylassana.tif22@polban.ac.id>
2024-05-04 23:46:11 +0700

    integrasi timer

--- lib/ui/views/timer.dart
+++ lib/ui/views/timer.dart
@@ -3,7 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:kuliahku/ui/shared/style.dart';
 import 'package:kuliahku/ui/widgets/button.dart';
 import 'package:kuliahku/ui/widgets/dropdown.dart';
-import 'dart:math' as math;
+import 'package:kuliahku/ui/shared/global.dart';
+import 'package:intl/intl.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import 'history_time_record.dart';
 
 class TimerPage extends StatefulWidget {
   const TimerPage({Key? key}) : super(key: key);
@@ -15,32 +20,18 @@ class TimerPage extends StatefulWidget {
 class _TimerPageState extends State<TimerPage> {
   int _seconds = 0;
   bool _isRunning = false;
+  bool _isFinish = false;
   late Timer _timer;
 
-  late int _selectedCourse;
-  late int _selectedLearningType;
+  late DateTime _startTime;
+  late DateTime _endTime;
 
-  void _startTimer() {
-    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
-      setState(() {
-        _seconds++;
-      });
-    });
-    _isRunning = true;
-  }
+  late int _selectedCourse = 1;
+  late int _selectedLearningType = 1;
 
-  void _stopTimer() {
-    _timer.cancel();
-    _isRunning = false;
-  }
-
-  void _resetTimer() {
-    _timer.cancel();
-    setState(() {
-      _seconds = 0;
-    });
-    _isRunning = false;
-  }
+  List<Map<String, dynamic>> jadwal = [];
+  int type = 0;
+  int subjectId = 0;
 
   String _formattedTime(int seconds) {
     int hours = seconds ~/ 3600;
@@ -58,6 +49,123 @@ class _TimerPageState extends State<TimerPage> {
   void dispose() {
     _timer.cancel();
     super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // fetchDataJadwal();
+  }
+
+  Future<void> fetchDataJadwal() async {
+    try {
+      String url = 'http://$ipUrl:8001/users/$email/jadwalKuliah/now';
+
+      var response = await http.get(
+        Uri.parse(url),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final List<dynamic> fetchedData = json.decode(response.body);
+        List<Map<String, dynamic>> transformedData = fetchedData.map((item) {
+          return {
+            'id': item['id'].toString(),
+            'nama matkul': item['subject'],
+          };
+        }).toList();
+
+        setState(() {
+          jadwal = transformedData;
+        });
+      } else {
+        throw Exception('Failed to load data');
+      }
+
+    } catch (error) {
+      print('Error fetching data: $error');
+    }
+  }
+
+  void addDataToBackend() async {
+    try {
+      // Data yang akan dikirim ke backend
+      Map<String, dynamic> data = {
+        'startTime': DateFormat('HH:mm:ss').format(_startTime),
+        'endTime': DateFormat('HH:mm:ss').format(_endTime),
+        'subject': _selectedCourse,
+        'jenis': _selectedLearningType,
+        'time_records': _formattedTime(_seconds),
+      };
+
+      String url = 'http://$ipUrl:8001/users/$email/time-records';
+      var response = await http.post(
+        Uri.parse(url),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode(data),
+      );
+
+      print(data);
+
+      if (response.statusCode == 201) {
+        print('Time record added successfully');
+
+        setState(() {
+          _isFinish = false;
+          _isRunning = false;
+          _seconds = 0;
+        });
+
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const HistoryRecordPage()),
+        );
+      } else {
+        throw Exception('Failed to add time record');
+      }
+    } catch (error) {
+      print('Error adding time record: $error');
+    }
+  }
+
+  void _startTimer() {
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        _seconds++;
+      });
+    });
+    setState(() {
+      _isRunning = true;
+      if(_seconds<=0)
+        _startTime = DateTime.now();
+    });
+  }
+
+  void _pauseTimer() {
+    _timer.cancel();
+    setState(() {
+      _isRunning = false;
+    });
+  }
+
+  void _stopTimer() {
+    _timer.cancel();
+    setState(() {
+      _isFinish = true;
+      _endTime = DateTime.now();
+    });
+  }
+
+  void _resetTimer() {
+    _timer.cancel();
+    setState(() {
+      _seconds = 0;
+      _isRunning = false;
+    });
   }
 
   @override
@@ -102,80 +210,94 @@ class _TimerPageState extends State<TimerPage> {
                 ),
               ],
             ),
-            SizedBox(height: 30),
-            _isRunning
+            SizedBox(height: 20),
+            _isRunning || _seconds>0
                 ? Column(
-                    children: [
-                      Text(
-                        'Mata Kuliah: $_selectedCourse',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      Text(
-                        'Jenis Belajar: $_selectedLearningType',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                    ],
-                  )
+              children: [
+                Text(
+                  'Mata Kuliah: $_selectedCourse',
+                  style: TextStyle(fontSize: 20),
+                ),
+                Text(
+                  'Jenis Belajar: $_selectedLearningType',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ],
+            )
                 : Column(
-                    children: [
-                      CustomDropdown(
-                        label: "Mata Kuliah",
-                        placeholder: "Pilih mata kuliah",
-                        items: [
-                          {'label': 'Basis Data', 'value': 1},
-                          {
-                            'label': 'Matematika',
-                            'value': 2
-                          } //nanti valuenya id nya
-                        ],
-                        onChanged: (value) {
-                          setState(() {
-                            _selectedCourse = value;
-                          });
-                        },
-                      ),
-                      SizedBox(height: 5),
-                      CustomDropdown(
-                        label: "Jenis Belajar",
-                        placeholder: "Pilih jenis belajar",
-                        items: [
-                          {
-                            'label': 'Mengerjakan Tugas',
-                            'value': 1
-                          },
-                          {
-                            'label': 'Belajar Mandiri',
-                            'value': 2
-                          },
-                        ],
-                        onChanged: (value) {
-                          setState(() {
-                            _selectedLearningType = value;
-                          });
-                        },
-                      ),
-                    ],
-                  ),
+              children: [
+                CustomDropdown(
+                  label: "Apa yang akan kamu kerjakan",
+                  placeholder: "Pilih jenis belajar",
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedLearningType = value;
+                    });
+                  },
+                  items: [
+                    {'label': 'Mengerjakan Tugas', 'value': 1},
+                    {'label': 'Belajar Mandiri', 'value': 2}
+                  ],
+                ),
+                CustomDropdown(
+                  label: "Mata Kuliah",
+                  placeholder: "Pilih mata kuliah",
+                  onChanged: (value) {
+                    setState(() {
+                      // _selectedCourse = jadwal[value]['id'];
+                      _selectedCourse = value;
+                    });
+                  },
+                  // items: List.generate(jadwal.length, (index) {
+                  //   return {
+                  //     'label': jadwal[index]['nama matkul'],
+                  //     'value': jadwal[index]['id']
+                  //   };
+                  // }),
+                  items: [
+                    {'label': 'Mengerjakan Tugas', 'value': 1},
+                    {'label': 'Belajar Mandiri', 'value': 2}
+                  ],
+                ),
+              ],
+            ),
             SizedBox(height: 30),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                CustomIconsButton(
-                  label: _isRunning ? 'Stop' : 'Start',
-                  iconData: _isRunning ? Icons.stop : Icons.play_arrow,
-                  onPressed: _isRunning ? _stopTimer : _startTimer,
-                  textColor: Colors.lightBlue,
-                  backgroundColor: Colors.white,
-                ),
-                SizedBox(width: 20),
-                if (_isRunning) // Tambahkan kondisi ini
+                if(_isFinish)
+                  CustomButton(
+                    backgroundColor: yellow,
+                    label: 'Simpan',
+                    textColor: white,
+                    onPressed: () {
+                      addDataToBackend();
+                    },
+                  ),
+                if (!_isFinish)
                   CustomIconsButton(
-                    onPressed: _resetTimer,
-                    iconData: Icons.refresh,
-                    label: 'Reset',
+                    label: _isRunning ? 'Pause' : 'Start',
+                    iconData: _isRunning ? Icons.pause: Icons.play_arrow,
+                    onPressed: _isRunning ? _pauseTimer : _startTimer,
                     textColor: Colors.lightBlue,
                     backgroundColor: Colors.white,
                   ),
+                  if (_isRunning && !_isFinish)
+                    CustomIconsButton(
+                      label: 'Stop',
+                      iconData: Icons.stop ,
+                      onPressed: _stopTimer ,
+                      textColor: Colors.lightBlue,
+                      backgroundColor: Colors.white,
+                    ),
+                  if (!_isRunning && !_isFinish && _seconds > 0)
+                    CustomIconsButton(
+                      onPressed: _resetTimer,
+                      iconData: Icons.refresh,
+                      label: 'Reset',
+                      textColor: Colors.lightBlue,
+                      backgroundColor: Colors.white,
+                    ),
               ],
             ),
           ],
