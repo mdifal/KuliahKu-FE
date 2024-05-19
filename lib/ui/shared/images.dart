4a7286c3498bb53a2736f7434690da5f39b5bbf7
Fauzanaylassana98 <fauza.naylassana.tif22@polban.ac.id>
2024-05-19 22:44:05 +0700

    tambah assets image

--- lib/ui/shared/images.dart
+++ lib/ui/shared/images.dart
@@ -2,5 +2,6 @@
 const String background_landing = "assets/images/gambar_landingpage.png";
 const String image_new_semester = "assets/images/gambar_addnewsemester.png";
 const String image_laporan_akhir = "assets/images/gambar_laporanbelajar.png";
+const String image_person = "assets/images/person.svg";
 
 // Icons
