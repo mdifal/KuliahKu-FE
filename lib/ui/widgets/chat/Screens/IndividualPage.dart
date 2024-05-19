3c65249e5e7f250d66ea6bed4d26864b7faf002d
Fauzanaylassana98 <fauza.naylassana.tif22@polban.ac.id>
2024-05-19 22:43:22 +0700

    membuat tampilan chat

---
+++ lib/ui/widgets/chat/Screens/IndividualPage.dart
@@ -0,0 +1,274 @@
+import 'package:camera/camera.dart';
+import 'package:kuliahku/ui/widgets/chat/CustomUI/OwnMessgaeCrad.dart';
+import 'package:kuliahku/ui/widgets/chat/CustomUI/ReplyCard.dart';
+import 'package:kuliahku/ui/widgets/chat/Model/ChatModel.dart';
+import 'package:kuliahku/ui/widgets/chat/Model/MessageModel.dart';
+import 'package:kuliahku/ui/shared/style.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+class IndividualPage extends StatefulWidget {
+  IndividualPage({Key? key, required this.chatModel}) : super(key: key);
+  final ChatModel chatModel;
+
+  @override
+  _IndividualPageState createState() => _IndividualPageState();
+}
+
+class _IndividualPageState extends State<IndividualPage> {
+  bool show = false;
+  FocusNode focusNode = FocusNode();
+  bool sendButton = false;
+  List<MessageModel> messages = [];
+  TextEditingController _controller = TextEditingController();
+  ScrollController _scrollController = ScrollController();
+  late IO.Socket socket;
+  @override
+  void initState() {
+    super.initState();
+    // connect();
+
+    focusNode.addListener(() {
+      if (focusNode.hasFocus) {
+        setState(() {
+          show = false;
+        });
+      }
+    });
+    connect();
+  }
+
+  void connect() {
+    // MessageModel messageModel = MessageModel(sourceId: widget.sourceChat.id.toString(),targetId: );
+    socket = IO.io("http://192.168.0.106:5000", <String, dynamic>{
+      "transports": ["websocket"],
+      "autoConnect": false,
+    });
+    socket.connect();
+    socket.onConnect((data) {
+      print("Connected");
+      socket.on("message", (msg) {
+        print(msg);
+        setMessage("destination", msg["message"]);
+        _scrollController.animateTo(_scrollController.position.maxScrollExtent,
+            duration: Duration(milliseconds: 300), curve: Curves.easeOut);
+      });
+    });
+    print(socket.connected);
+  }
+
+  void sendMessage(String message, int targetId) {
+    setMessage("source", message);
+    socket.emit("message",
+        {"message": message, "targetId": targetId});
+  }
+
+  void setMessage(String type, String message) {
+    MessageModel messageModel = MessageModel(
+        type: type,
+        message: message,
+        time: DateTime.now().toString().substring(10, 16));
+    print(messages);
+
+    setState(() {
+      messages.add(messageModel);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Image.asset(
+          "assets/whatsapp_Back.png",
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          fit: BoxFit.cover,
+        ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: PreferredSize(
+            preferredSize: Size.fromHeight(60),
+            child: AppBar(
+              leadingWidth: 70,
+              titleSpacing: 0,
+              leading: InkWell(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.arrow_back,
+                      size: 24,
+                    ),
+                    CircleAvatar(
+                      child: SvgPicture.asset(
+                        widget.chatModel.isGroup
+                            ? "assets/groups.svg"
+                            : "assets/person.svg",
+                        color: Colors.white,
+                        height: 36,
+                        width: 36,
+                      ),
+                      radius: 20,
+                      backgroundColor: Colors.blueGrey,
+                    ),
+                  ],
+                ),
+              ),
+              title: InkWell(
+                onTap: () {},
+                child: Container(
+                  margin: EdgeInsets.all(6),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.chatModel.name,
+                        style: TextStyle(
+                          fontSize: 18.5,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          body: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: WillPopScope(
+              child: Column(
+                children: [
+                  Expanded(
+                    // height: MediaQuery.of(context).size.height - 150,
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      controller: _scrollController,
+                      itemCount: messages.length + 1,
+                      itemBuilder: (context, index) {
+                        if (index == messages.length) {
+                          return Container(
+                            height: 70,
+                          );
+                        }
+                        if (messages[index].type == "source") {
+                          return OwnMessageCard(
+                            message: messages[index].message,
+                            time: messages[index].time,
+                          );
+                        } else {
+                          return ReplyCard(
+                            message: messages[index].message,
+                            time: messages[index].time,
+                          );
+                        }
+                      },
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      height: 70,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Row(
+                            children: [
+                              Container(
+                                width: MediaQuery.of(context).size.width - 60,
+                                child: Card(
+                                  margin: EdgeInsets.only(
+                                      left: 2, right: 2, bottom: 8),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(25),
+                                  ),
+                                  child: TextFormField(
+                                    controller: _controller,
+                                    focusNode: focusNode,
+                                    textAlignVertical: TextAlignVertical.center,
+                                    keyboardType: TextInputType.multiline,
+                                    maxLines: 5,
+                                    minLines: 1,
+                                    onChanged: (value) {
+                                      if (value.length > 0) {
+                                        setState(() {
+                                          sendButton = true;
+                                        });
+                                      } else {
+                                        setState(() {
+                                          sendButton = false;
+                                        });
+                                      }
+                                    },
+                                    decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: "Type a message",
+                                      hintStyle: TextStyle(color: greySoft),
+                                      suffixIcon: Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          IconButton(
+                                            icon: Icon(Icons.send),
+                                            onPressed: () {
+                                                _scrollController.animateTo(
+                                                    _scrollController
+                                                        .position.maxScrollExtent,
+                                                    duration:
+                                                    Duration(milliseconds: 300),
+                                                    curve: Curves.easeOut);
+                                                sendMessage(
+                                                    _controller.text,
+                                                    widget.chatModel.id);
+                                                _controller.clear();
+                                            },
+                                          ),
+                                        ],
+                                      ),
+                                      contentPadding: EdgeInsets.all(5),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                  bottom: 8,
+                                  right: 2,
+                                  left: 2,
+                                ),
+                                child: CircleAvatar(
+                                  radius: 25,
+                                  backgroundColor: greySoft,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              onWillPop: () {
+                if (show) {
+                  setState(() {
+                    show = false;
+                  });
+                } else {
+                  Navigator.pop(context);
+                }
+                return Future.value(false);
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/ui/widgets/chat/Screens/SelectContact.dart
@@ -0,0 +1,118 @@
+import 'package:kuliahku/ui/widgets/chat/CustomUI/ButtonCard.dart';
+import 'package:kuliahku/ui/widgets/chat/CustomUI/ContactCard.dart';
+import 'package:kuliahku/ui/widgets/chat/Model/ChatModel.dart';
+import 'package:kuliahku/ui/widgets/chat/Screens/CreateGroup.dart';
+import 'package:flutter/material.dart';
+
+class SelectContact extends StatefulWidget {
+  SelectContact({Key? key}) : super(key: key);
+
+  @override
+  _SelectContactState createState() => _SelectContactState();
+}
+
+class _SelectContactState extends State<SelectContact> {
+  late List<ChatModel> contacts;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    contacts = List.generate(
+      10,
+          (index) => ChatModel(
+        id: index,
+        name: 'User $index',
+        currentMessage: 'Hello, this is message $index',
+        time: '10:00 AM',
+        icon: 'https://via.placeholder.com/150',
+        isGroup: false,
+        status: "123",
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+     return Scaffold(
+        appBar: AppBar(
+          title: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Select Contact",
+                style: TextStyle(
+                  fontSize: 19,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "256 contacts",
+                style: TextStyle(
+                  fontSize: 13,
+                ),
+              )
+            ],
+          ),
+          actions: [
+            IconButton(
+                icon: Icon(
+                  Icons.search,
+                  size: 26,
+                ),
+                onPressed: () {}),
+            PopupMenuButton<String>(
+              padding: EdgeInsets.all(0),
+              onSelected: (value) {
+                print(value);
+              },
+              itemBuilder: (BuildContext contesxt) {
+                return [
+                  PopupMenuItem(
+                    child: Text("Invite a friend"),
+                    value: "Invite a friend",
+                  ),
+                  PopupMenuItem(
+                    child: Text("Contacts"),
+                    value: "Contacts",
+                  ),
+                  PopupMenuItem(
+                    child: Text("Refresh"),
+                    value: "Refresh",
+                  ),
+                  PopupMenuItem(
+                    child: Text("Help"),
+                    value: "Help",
+                  ),
+                ];
+              },
+            ),
+          ],
+        ),
+        body: ListView.builder(
+            itemCount: contacts.length + 2,
+            itemBuilder: (context, index) {
+              if (index == 0) {
+                return InkWell(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (builder) => CreateGroup()));
+                  },
+                  child: ButtonCard(
+                    icon: Icons.group,
+                    name: "New group",
+                  ),
+                );
+              } else if (index == 1) {
+                return ButtonCard(
+                  icon: Icons.person_add,
+                  name: "New contact",
+                );
+              }
+              return ContactCard(
+                contact: contacts[index - 2],
+              );
+            }));
+  }
+}
