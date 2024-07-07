96e8b9f6e4f92e5da2c19a98a40c2b6f1e1607fc
Fauzanaylassana98 <fauza.naylassana.tif22@polban.ac.id>
2024-07-07 15:53:06 +0700

    update chatmodel

--- lib/ui/widgets/chat/Model/ChatModel.dart
+++ lib/ui/widgets/chat/Model/ChatModel.dart
@@ -1,20 +1,22 @@
 class ChatModel {
+  String id;
+  String targetId;
   String name;
-  String icon;
-  bool isGroup;
+  String profilePicture;
   String time;
   String currentMessage;
   String status;
   bool select = false;
-  int id;
   ChatModel({
+    required this.id,
+    required this.targetId,
     required this.name,
-    required this.icon,
-    required this.isGroup,
+    required this.profilePicture,
     required this.time,
     required this.currentMessage,
     required this.status,
     this.select = false,
-    required this.id,
   });
+
+  static fromJson(data) {}
 }
