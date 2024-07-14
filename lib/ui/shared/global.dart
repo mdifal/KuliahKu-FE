0d5f97513a3fa5c5ec615e2b69aea9421e11d2f4
Fauzanaylassana98 <fauza.naylassana.tif22@polban.ac.id>
2024-07-14 23:10:21 +0700

    hapus email

--- lib/ui/shared/global.dart
+++ lib/ui/shared/global.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 // variabel
 const int userId = 1;
-String email = 'rawr@email.com';
+String email = '';
 const String ipUrl = 'goldenrod-careful-lettuce.glitch.me';
 String idSemester = '';
