258cda975cfcd173a91141232d49a4215768d338
Fauzanaylassana98 <fauza.naylassana.tif22@polban.ac.id>
2024-07-21 14:48:38 +0700

    isi email

--- lib/ui/shared/global.dart
+++ lib/ui/shared/global.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 // variabel
 const int userId = 1;
-String email = '';
+String email = 'rawr@email.com';
 const String ipUrl = 'goldenrod-careful-lettuce.glitch.me';
+// const String ipUrl = '192.168.100.28:8001';
 String idSemester = '';
