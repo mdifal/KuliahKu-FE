8f1b6d391c3d626d4507cafd9db4142d902ddece
nisrinawafaz <nisrinawafaz@gmail.com>
2024-07-30 22:42:46 +0700

    add widget timepicker

---
+++ lib/ui/widgets/timePicker.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+Future<TimeOfDay?> showTimePicker({
+  required BuildContext context,
+  required TimeOfDay initialTime,
+  TransitionBuilder? builder,
+  bool barrierDismissible = true,
+  Color? barrierColor,
+  String? barrierLabel,
+  bool useRootNavigator = true,
+  TimePickerEntryMode initialEntryMode = TimePickerEntryMode.dial,
+  String? cancelText,
+  String? confirmText,
+  String? helpText,
+  String? errorInvalidText,
+  String? hourLabelText,
+  String? minuteLabelText,
+  RouteSettings? routeSettings,
+  EntryModeChangeCallback? onEntryModeChanged,
+  Offset? anchorPoint,
+  Orientation? orientation,
+}) async {
+  assert(debugCheckHasMaterialLocalizations(context));
+
+  final Widget dialog = TimePickerDialog(
+    initialTime: initialTime,
+    initialEntryMode: initialEntryMode,
+    cancelText: cancelText,
+    confirmText: confirmText,
+    helpText: helpText,
+    errorInvalidText: errorInvalidText,
+    hourLabelText: hourLabelText,
+    minuteLabelText: minuteLabelText,
+    orientation: orientation,
+    onEntryModeChanged: onEntryModeChanged,
+  );
+  return showDialog<TimeOfDay>(
+    context: context,
+    barrierDismissible: barrierDismissible,
+    barrierColor: barrierColor,
+    barrierLabel: barrierLabel,
+    useRootNavigator: useRootNavigator,
+    builder: (BuildContext context) {
+      return builder == null ? dialog : builder(context, dialog);
+    },
+    routeSettings: routeSettings,
+    anchorPoint: anchorPoint,
+  );
+}
